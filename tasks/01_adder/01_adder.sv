a4f4229f22b0f09ee0d2d4b467d5855065f922bd
Mike Kuskov <m.kuskov@innopolis.university>
2025-11-24 01:57:40 +0300

    Update task 1

--- tasks/01_adder/01_adder.sv
+++ tasks/01_adder/01_adder.sv
@@ -1,8 +1,8 @@
 module adder (
-    input  [4:0] a,
-    input  [4:0] b,
+    input  a4, a3, a2, a1, a0,
+    input  b4, b3, b2, b1, b0,
 
-    output [5:0] out
+    output q5, q4, q3, q2, q1, q0
 );
-    assign out = a + b;
+
 endmodule
